d2a065eef9b2af0728e42aabdb5f4b05bc4de06e
khushi173 <khushijamwal173@gmail.com>
2022-03-29 15:10:15 +0530

    updated

--- test_main.py
+++ test_main.py
@@ -1,40 +1,30 @@
 import main
+import pytest
+
+#@pytest.fixture
+#def input():
+ #   a = 315
+  #  return a
 
 
-def test_divby5_1():
-    a = 25
-    result = main.divby5(a)
-    assert result == True
+@pytest.mark.parametrize("num,output",[(5,True),(2,False),(10,True),(7,False)])
+def test_divby5_1(num,output):
+    result = main.divby5(num)
+    assert result == output
 
-def test_divby5_2():
-        a = 19
-        result = main.divby5(a)
-        assert result == False
 
-def test_divby7_1():
-         a = 14
-         result = main.divby7(a)
-         assert result == True
+@pytest.mark.parametrize("num,output",[(14,True),(20,False),(49,True),(2,False)])
+def test_divby7_1(num,output):
+         result = main.divby7(num)
+         assert result == output
 
-def test_divby5_2():
-        a = 3
-        result = main.divby7(a)
-        assert result == False
-def test_divby9_1():
-    a = 54
-    result = main.divby9(a)
-    assert result == True
+@pytest.mark.parametrize("num,output",[(45,True),(12,False),(90,True),(7,False)])
+def test_divby9_1(num,output):
+    result = main.divby9(num)
+    assert result == output
 
-def test_divby9_2():
-        a = 19
-        result = main.divby9(a)
-        assert result == False
-def test_divby11_1():
-    a = 121
-    result = main.divby11(a)
-    assert result == True
+@pytest.mark.parametrize("num,output",[(121,True),(12,False),(110,True),(7,False)])
+def test_divby11_1(num,output):
+    result = main.divby11(num)
+    assert result == output
 
-def test_divby11_2():
-        a = 12
-        result = main.divby11(a)
-        assert result == False
